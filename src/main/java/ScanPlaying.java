8d88f55d7ef44a154d445cbf689962de8f975bbe
Václav Juřena <vaclav.jurena@inventi.cz>
2022-06-16 10:20:03 +0200

    add calculation of triangle area

--- src/main/java/ScanPlaying.java
+++ src/main/java/ScanPlaying.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ScanPlaying {
@@ -63,16 +64,76 @@ public class ScanPlaying {
 
     }
 
+    public void stringParseInt(String a, String b){
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Insert first number: ");
+        a = sc.nextLine();
+        System.out.println("Insert second number: ");
+        b = sc.nextLine();
+       int num = Integer.parseInt(a);
+       int num2 = Integer.parseInt(b);
+
+        System.out.println("Inserted numbers are:" + num + "/" + num2);
+        }
 
 
+        public void calculateTriangleArea() {
+            double a;
+            double b;
+            double c = 0;
 
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Please, enter A side of a triangle: ");
+            while (true) {
+                try {
+                    a = Double.parseDouble(sc.nextLine());
+                    break;
+                } catch (NumberFormatException nfe) {
+                    System.out.print("Invalid number try again: ");
+                }
+            }
+
+            System.out.print("Please, enter B side of a triangle: ");
+            while (true) {
+                try {
+                    b = Double.parseDouble(sc.nextLine());
+                    break;
+                } catch (NumberFormatException nfe) {
+                    System.out.print("Invalid number try again: ");
+
+                }
+            }
+
+            System.out.print("Please, enter C side of a triangle: ");
+            while (true) {
+                try {
+                    c = Double.parseDouble(sc.nextLine());
+                    break;
+                } catch (NumberFormatException nfe) {
+                    System.out.print("Invalid number try again: ");
+
+                }
+            }
+                double p = (a + b + c) / 2;
+                System.out.println(p);
+                double firstside = p - a;
+                double secondside = p - b;
+                double thirdside = p - c;
+                double area = Math.sqrt(p * firstside * secondside * thirdside);
+
+                System.out.println("Triangle area is: " + area);
+            }
 
 
     public static void main (String[]args){
                     ScanPlaying scanPlaying = new ScanPlaying();
-        //          scanPlaying.theGreatest();
-                    scanPlaying.sum2Numbers(2,3);
+                  //    scanPlaying.theGreatest();
+                //    scanPlaying.sum2Numbers(2,3);
+               //     scanPlaying.stringParseInt("3" ,"2");
+                    scanPlaying.calculateTriangleArea();
                 }
             }
+
+
 
 
